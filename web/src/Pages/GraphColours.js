44e3ad417441e0afc7c1534b18536c75c8322023
isobelm <isobel.mahon@gmail.com>
2019-09-13 12:36:56 +0100

    actually CoLOurS

---
+++ web/src/Pages/GraphColours.js
@@ -0,0 +1,36 @@
+const GraphColours = {
+    "HR":{
+        "Donal": "#F12711",
+        "Ebin": "#BB8967",
+        "Gemma": "#FFD8A4",
+        "Isobel": "#FFA69D",
+        "Niall": "#921C10",
+        "Rory": "#FFAA00",
+    },
+    "MISTAKE":{
+        "Donal": "#1111100",
+        "Ebin": "#332233",
+        "Gemma": "#445555",
+        "Isobel": "#777788",
+        "Niall": "#99aa99",
+        "Rory": "#ccbbbb",
+    },
+    "Yike":{
+        "Donal": "#00ff00",
+        "Ebin": "#631FBF",
+        "Gemma": "#888F00",
+        "Isobel": "#CCBBA4",
+        "Niall": "#000000",
+        "Rory": "#51D095",
+    },
+    "Goof Chold":{
+        "Donal": "#900086",
+        "Ebin": "#FF007B",
+        "Gemma": "#FF3E3E",
+        "Isobel": "#E6ABFF",
+        "Niall": "#250048",
+        "Rory": "#FF7575",
+    },
+}
+
+export default GraphColours
